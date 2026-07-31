9115316aa8c06978de5a6941f4d3ab2d9b24220e
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-31 21:20:07 +0300

    fix:update the app.py file by adding the elif function

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import random
 number = random.randint(1, 100)
 
 guess = int(input("Guess a number between 1 and 100: "))
+
+if guess == number:
+    print("Congratulations! You guessed the number!")
+elif guess < number:
+    print("Too Low!")
+else:
+    print("Too High!")
